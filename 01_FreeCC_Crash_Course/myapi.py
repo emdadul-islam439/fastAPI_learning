ea83b31a93a16e643da738dd561ba9754e20bcae
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-08 13:31:00 +0600

    created our first Path API get_student(...)

--- 01_FreeCC_Crash_Course/myapi.py
+++ 01_FreeCC_Crash_Course/myapi.py
@@ -2,8 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "Name" : "Student 1",
+        "age" : "20",
+        "class" : "year 11",
+    },
+    2: {
+        "Name" : "Student 2",
+        "age" : "22",
+        "class" : "year 13",
+    }
+}
+
+# api end-point without any parameter
 @app.get("/")
 def index():
     return {
         "name" : "First Data"
     }
+
+# api end-point with path-parameter
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    if student_id in students:
+        return students[student_id]
+    else:
+        return {"data" : "Not found"}
